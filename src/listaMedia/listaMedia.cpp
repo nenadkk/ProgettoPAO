756ba4ca052a0210106c849b6ab2f81e13d0117b
nenadkk <nnrdlvc@gmail.com>
2025-03-11 16:45:04 +0100

    Iniziata parte gestione ListaMedia: save, load, alcuni filtri, search generico. Da fare add, remove ed edit.

---
+++ src/listaMedia/listaMedia.cpp
@@ -0,0 +1,96 @@
+#include "listaMedia.h"
+#include "../jsonHandler/jsonVisitor.h"
+#include <string>
+void listamedia::save() const
+{
+    jsonVisitor jVisitor; 
+    jVisitor.clearAll();
+    
+    for(auto m : LM)
+        m->accept(&jVisitor);
+}
+
+void listamedia::load()
+{
+    jsonVisitor jVisitor;
+    jVisitor.readAllLibri(LM);
+    jVisitor.readAllCanzoni(LM);
+    jVisitor.readAllAlbum(LM);
+}
+
+list<media*> listamedia::filtroSoloLibri() const
+{
+    list<media*> listaTemp;
+    for(auto m : LM)
+    {
+        if(dynamic_cast<libro*>(m))
+            listaTemp.push_back(m);
+    }
+    return listaTemp;
+}
+
+list<media*> listamedia::filtroSoloCanzoni() const
+{
+    list<media*> listaTemp;
+    for(auto m : LM)
+    {
+        if(dynamic_cast<canzone*>(m))
+            listaTemp.push_back(m);
+    }
+    return listaTemp;
+}
+
+list<media*> listamedia::filtroSoloAlbum() const
+{
+    list<media*> listaTemp;
+    for(auto m : LM)
+    {
+        if(dynamic_cast<album*>(m))
+            listaTemp.push_back(m);
+    }
+    return listaTemp;
+}
+
+list<media*> listamedia::search(string& strDaCercare) const
+{
+    list<media*> listaTemp;
+    for(auto m : LM)
+    {
+        if(m->getTitolo().find(strDaCercare) != string::npos ||
+           m->getAutore().find(strDaCercare) != string::npos || 
+           to_string(m->getAnno()).find(strDaCercare) != string::npos ||
+           to_string(m->getId()).find(strDaCercare) != string::npos)
+        {
+            listaTemp.push_back(m);
+        }
+        else if (auto t = dynamic_cast<libro*>(m)) 
+        {
+            if(to_string(t->getIsbn()).find(strDaCercare) != string::npos || t->getEditore().find(strDaCercare) != string::npos)
+                listaTemp.push_back(m);
+        }
+        else if (auto t = dynamic_cast<canzone*>(m)) 
+        {
+            if(t->getGenere().find(strDaCercare) != string::npos)
+                listaTemp.push_back(m);
+        }
+
+    }
+
+    return listaTemp;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

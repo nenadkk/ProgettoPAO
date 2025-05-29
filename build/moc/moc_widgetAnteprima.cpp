d43d1c92793068f36ef157d75b7ff77400119f38
nenadkk <nnrdlvc@gmail.com>
2025-05-29 11:41:29 +0200

    Trovato errore che se l'immagine selezionata è nella cartella ./immagini mette il path completo e non relativo

--- build/moc/moc_widgetAnteprima.cpp
+++ build/moc/moc_widgetAnteprima.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'widgetAnteprima.h'
 **
-** Created by: The Qt Meta Object Compiler version 68 (Qt 6.4.2)
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.4)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -9,45 +9,37 @@
 #include <memory>
 #include "../../src/grafica/widgetAnteprima.h"
 #include <QtGui/qtextcursor.h>
+#include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'widgetAnteprima.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 68
-#error "This file was generated using the moc from 6.4.2. It"
+#error "This file was generated using the moc from 6.2.4. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
-#endif
-
-#ifndef Q_CONSTINIT
-#define Q_CONSTINIT
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
-namespace {
 struct qt_meta_stringdata_widgetAnteprima_t {
-    uint offsetsAndSizes[6];
-    char stringdata0[16];
-    char stringdata1[8];
-    char stringdata2[1];
+    const uint offsetsAndSize[6];
+    char stringdata0[25];
 };
 #define QT_MOC_LITERAL(ofs, len) \
-    uint(sizeof(qt_meta_stringdata_widgetAnteprima_t::offsetsAndSizes) + ofs), len 
-Q_CONSTINIT static const qt_meta_stringdata_widgetAnteprima_t qt_meta_stringdata_widgetAnteprima = {
+    uint(offsetof(qt_meta_stringdata_widgetAnteprima_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_widgetAnteprima_t qt_meta_stringdata_widgetAnteprima = {
     {
-        QT_MOC_LITERAL(0, 15),  // "widgetAnteprima"
-        QT_MOC_LITERAL(16, 7),  // "clicked"
-        QT_MOC_LITERAL(24, 0)   // ""
+QT_MOC_LITERAL(0, 15), // "widgetAnteprima"
+QT_MOC_LITERAL(16, 7), // "clicked"
+QT_MOC_LITERAL(24, 0) // ""
+
     },
-    "widgetAnteprima",
-    "clicked",
-    ""
+    "widgetAnteprima\0clicked\0"
 };
 #undef QT_MOC_LITERAL
-} // unnamed namespace
 
-Q_CONSTINIT static const uint qt_meta_data_widgetAnteprima[] = {
+static const uint qt_meta_data_widgetAnteprima[] = {
 
  // content:
       10,       // revision
@@ -69,21 +61,6 @@ Q_CONSTINIT static const uint qt_meta_data_widgetAnteprima[] = {
        0        // eod
 };
 
-Q_CONSTINIT const QMetaObject widgetAnteprima::staticMetaObject = { {
-    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
-    qt_meta_stringdata_widgetAnteprima.offsetsAndSizes,
-    qt_meta_data_widgetAnteprima,
-    qt_static_metacall,
-    nullptr,
-    qt_incomplete_metaTypeArray<qt_meta_stringdata_widgetAnteprima_t,
-        // Q_OBJECT / Q_GADGET
-        QtPrivate::TypeAndForceComplete<widgetAnteprima, std::true_type>,
-        // method 'clicked'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>
-    >,
-    nullptr
-} };
-
 void widgetAnteprima::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
@@ -97,7 +74,7 @@ void widgetAnteprima::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int 
         int *result = reinterpret_cast<int *>(_a[0]);
         {
             using _t = void (widgetAnteprima::*)();
-            if (_t _q_method = &widgetAnteprima::clicked; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&widgetAnteprima::clicked)) {
                 *result = 0;
                 return;
             }
@@ -105,6 +82,22 @@ void widgetAnteprima::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int 
     }
     (void)_a;
 }
+
+const QMetaObject widgetAnteprima::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_widgetAnteprima.offsetsAndSize,
+    qt_meta_data_widgetAnteprima,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_widgetAnteprima_t
+, QtPrivate::TypeAndForceComplete<widgetAnteprima, std::true_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+
+>,
+    nullptr
+} };
+
 
 const QMetaObject *widgetAnteprima::metaObject() const
 {
--- build/moc/moc_widgetEditorMedia.cpp
+++ build/moc/moc_widgetEditorMedia.cpp
@@ -1,59 +1,47 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'widgetEditorMedia.h'
 **
-** Created by: The Qt Meta Object Compiler version 68 (Qt 6.4.2)
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.4)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
 #include <memory>
 #include "../../src/grafica/widgetEditorMedia.h"
-#include <QtGui/qtextcursor.h>
+#include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'widgetEditorMedia.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 68
-#error "This file was generated using the moc from 6.4.2. It"
+#error "This file was generated using the moc from 6.2.4. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
-#endif
-
-#ifndef Q_CONSTINIT
-#define Q_CONSTINIT
 #endif
 
 QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
-namespace {
 struct qt_meta_stringdata_widgetEditorMedia_t {
-    uint offsetsAndSizes[10];
-    char stringdata0[18];
-    char stringdata1[5];
-    char stringdata2[1];
-    char stringdata3[9];
-    char stringdata4[12];
+    const uint offsetsAndSize[10];
+    char stringdata0[45];
 };
 #define QT_MOC_LITERAL(ofs, len) \
-    uint(sizeof(qt_meta_stringdata_widgetEditorMedia_t::offsetsAndSizes) + ofs), len 
-Q_CONSTINIT static const qt_meta_stringdata_widgetEditorMedia_t qt_meta_stringdata_widgetEditorMedia = {
+    uint(offsetof(qt_meta_stringdata_widgetEditorMedia_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_widgetEditorMedia_t qt_meta_stringdata_widgetEditorMedia = {
     {
-        QT_MOC_LITERAL(0, 17),  // "widgetEditorMedia"
-        QT_MOC_LITERAL(18, 4),  // "crea"
-        QT_MOC_LITERAL(23, 0),  // ""
-        QT_MOC_LITERAL(24, 8),  // "modifica"
-        QT_MOC_LITERAL(33, 11)   // "browseImage"
+QT_MOC_LITERAL(0, 17), // "widgetEditorMedia"
+QT_MOC_LITERAL(18, 4), // "crea"
+QT_MOC_LITERAL(23, 0), // ""
+QT_MOC_LITERAL(24, 8), // "modifica"
+QT_MOC_LITERAL(33, 11) // "browseImage"
+
     },
-    "widgetEditorMedia",
-    "crea",
-    "",
-    "modifica",
+    "widgetEditorMedia\0crea\0\0modifica\0"
     "browseImage"
 };
 #undef QT_MOC_LITERAL
-} // unnamed namespace
 
-Q_CONSTINIT static const uint qt_meta_data_widgetEditorMedia[] = {
+static const uint qt_meta_data_widgetEditorMedia[] = {
 
  // content:
       10,       // revision
@@ -79,25 +67,6 @@ Q_CONSTINIT static const uint qt_meta_data_widgetEditorMedia[] = {
        0        // eod
 };
 
-Q_CONSTINIT const QMetaObject widgetEditorMedia::staticMetaObject = { {
-    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
-    qt_meta_stringdata_widgetEditorMedia.offsetsAndSizes,
-    qt_meta_data_widgetEditorMedia,
-    qt_static_metacall,
-    nullptr,
-    qt_incomplete_metaTypeArray<qt_meta_stringdata_widgetEditorMedia_t,
-        // Q_OBJECT / Q_GADGET
-        QtPrivate::TypeAndForceComplete<widgetEditorMedia, std::true_type>,
-        // method 'crea'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'modifica'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'browseImage'
-        QtPrivate::TypeAndForceComplete<void, std::false_type>
-    >,
-    nullptr
-} };
-
 void widgetEditorMedia::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
@@ -112,6 +81,22 @@ void widgetEditorMedia::qt_static_metacall(QObject *_o, QMetaObject::Call _c, in
     }
     (void)_a;
 }
+
+const QMetaObject widgetEditorMedia::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_widgetEditorMedia.offsetsAndSize,
+    qt_meta_data_widgetEditorMedia,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_widgetEditorMedia_t
+, QtPrivate::TypeAndForceComplete<widgetEditorMedia, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
 
 const QMetaObject *widgetEditorMedia::metaObject() const
 {
